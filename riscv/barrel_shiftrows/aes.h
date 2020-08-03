e49010be070db098689837c89772f4dd7f44c689
Alexandre Adomnicai <alex.adomnicai@gmail.com>
2020-08-03 23:34:15 +0800

    Add RV32I AES-256 implementations

--- riscv/barrel_shiftrows/aes.h
+++ riscv/barrel_shiftrows/aes.h
@@ -3,8 +3,10 @@
 
 #include <stdint.h>
 
-extern void aes128_keyschedule_lut(uint32_t rkeys[352], const uint8_t key[16]);
+void aes128_keyschedule_lut(uint32_t rkeys[352], const uint8_t key[16]);
+void aes256_keyschedule_lut(uint32_t rkeys[480], const uint8_t key[32]);
 
-extern void aes128_encrypt(uint8_t out[128], const uint8_t in[128], const uint32_t rkeys[352]);
+void aes128_encrypt(uint8_t out[128], const uint8_t in[128], const uint32_t rkeys[352]);
+void aes256_encrypt(uint8_t out[128], const uint8_t in[128], const uint32_t rkeys[480]);
 
 #endif
